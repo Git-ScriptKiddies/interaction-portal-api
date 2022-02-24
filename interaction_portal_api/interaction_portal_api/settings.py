9e36e660bc369d72b40bf6618705706ca3ac8437
wickviru <mayank.20195135@mnnit.ac.in>
2022-02-24 10:10:02 +0530

    'addeddb'

--- interaction_portal_api/interaction_portal_api/settings.py
+++ interaction_portal_api/interaction_portal_api/settings.py
@@ -37,7 +37,20 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework.authtoken',
+    'candidate',
+    'company',
+    'jobs'
+
 ]
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated', )
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -75,8 +88,12 @@ WSGI_APPLICATION = 'interaction_portal_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'interaction',
+        'USER': 'wick',
+        'PASSWORD': 'Olsen2317#',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
